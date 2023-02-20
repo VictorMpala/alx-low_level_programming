2fa904fcc180df5223e69c2d2d1bdf2a84383cba
Victor Mpala <apexmedia1799@gmail.com>
2023-02-19 23:53:37 -0800

    hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main- print a line
+ * Return:0 if the function executes properly
  */
-int main(void)/*entry point of the programming*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-/**
- * return-success
- */
 	return (0);
 }
