c67af974c04f06a036a29892f42c6a8115232d91
Victor Mpala <apexmedia1799@gmail.com>
2023-02-20 02:54:28 -0800

    uniX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints a statement to std err
+ * Return : 1 if the program executes properly
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n"), stdout;
+	return (1);
+}
