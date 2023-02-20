e29203753a1ae0cfafeda8e0426fc4dbca561bdb
Victor Mpala <apexmedia1799@gmail.com>
2023-02-19 23:45:43 -0800

    helo-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@ int main(void)/*entry point of the programming*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 /**
- *
- * return 0 -success
+ * return-success
  */
 	return (0);
 }
