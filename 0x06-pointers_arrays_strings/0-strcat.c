febf0bf07db99c03460c2473a7f85d4bd08fe807
Victor Mpala <apexmedia1799@gmail.com>
2023-03-03 18:28:05 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,24 @@
 #include "main.h"
-/**
- *_strncat - function that concatenates two strings. it will use
- * at most n bytes from src. src does not need to be null terminated.
- *@dest: pointer to destination string.
- *@src: pointer to source string.
- *@n: number of bytes to be concatenated.
- *
- *Return: pointer to destination string.
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int length, j;
-/* j is a counter for  n bytes of src to be concatenated */
-/* length = length of destination string */
 
-	length = 0;
-	while (dest[length] != '\0')
+/**
+ * _strcat - concatenates two strings
+ * @src: string to copy
+ * @dest: copied string
+ * Return: a pointer to the resulting string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
 	{
-		length++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++, length++)
-	{
-		dest[length] = src[j];
-	}
-	dest[length] = '\0';
 	return (dest);
 }
