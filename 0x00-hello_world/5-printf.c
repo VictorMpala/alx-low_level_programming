32f5443b96de7423fa170bf9601fba5190255b75
VictorMpala <apexmedia1799@gmail.com>
2023-10-05 02:43:11 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main fctn
+ * main - main function of the program
+ * puts - fctn for printing
+ * Return:0 indicates success
+ */
+int main(void)
+{
+char *to_print = "Programming is like building a multilingual puzzle";
+puts(to_print);
+return (0);
+}
