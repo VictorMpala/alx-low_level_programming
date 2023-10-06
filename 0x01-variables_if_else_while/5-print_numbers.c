7baf061397c30dceda33f36cdb2948ece1ce6bb1
VictorMpala <apexmedia1799@gmail.com>
2023-10-06 06:18:45 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main fctn
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+int  alpha;
+for (alpha = 0; alpha <= 9; alpha++)
+{
+printf("%d", alpha);
+}
+printf("\n");
+return (0);
+}
+
