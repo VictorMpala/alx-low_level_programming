1f053bb66a1926a0cba48407358b40df4fb5f6ec
VictorMpala <apexmedia1799@gmail.com>
2023-10-06 05:44:48 -0700

    task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main  main fctn
+ *
+ *
+ *
+ *
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+char a = 'B';
+int b = 8;
+long c = 1029;
+long long int d = 4034;
+float e = 3.09;
+printf("Size of a char: %zu \n", sizeof(a));
+printf("Size of an int: %zu \n", sizeof(b));
+printf("Size of a long: %zu \n", sizeof(c));
+printf("Size of a long long int: %zu \n", sizeof(d));
+printf("Size of a float: %ld \n, sizeof(e)");
+return (0);
+}
