990a2414b4af321bfc84e5563bb7afaa48f35bf4
VictorMpala <apexmedia1799@gmail.com>
2023-10-06 06:22:15 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main fctn
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 48; alpha <= 57; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
+
