5c0eb45184f7f1a22ce0c628dbb39a5a7d567d6d
VictorMpala <apexmedia1799@gmail.com>
2023-10-06 06:03:04 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main fctn
+ * @alpha: character variable
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+	putchar(alpha);
+	putchar('\n');
+return (0);
+}
