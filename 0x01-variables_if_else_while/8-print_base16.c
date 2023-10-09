00fd08369ea4311746e1530e5c2dc583a0568388
VictorMpala <apexmedia1799@gmail.com>
2023-10-09 08:45:32 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main fctn
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 48; alpha <= 57; alpha++)
+{
+putchar(alpha);
+}
+for (alpha = 97; alpha <= 102; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
+
