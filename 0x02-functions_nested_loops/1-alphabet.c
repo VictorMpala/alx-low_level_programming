9efea56e92ed7077936135bfd99da33a174f6e12
VictorMpala <apexmedia1799@gmail.com>
2023-10-10 09:16:02 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main fctn
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+return (0);
+}
